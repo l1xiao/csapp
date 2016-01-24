46fec88304be7a01f953a38415b9be9c602a0f43
l1xiao <shutianhe@gmail.com>
2016-01-24 21:31:30 +0800

    finish level3

---
+++ buf lab/test.d
@@ -0,0 +1,11 @@
+
+test.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   5:	bd 70 35 68 55       	mov    $0x55683570,%ebp
+   a:	68 be 8d 04 08       	push   $0x8048dbe
+   f:	c3                   	ret    
